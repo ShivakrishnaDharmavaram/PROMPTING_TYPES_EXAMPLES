475ada31f0820300da750f93d883277104417262
ShivakrishnaD <shivakrishnadss1999@gmail.com>
2025-12-17 17:27:53 +0530

    updated prompt

--- cot_prompt.py
+++ cot_prompt.py
@@ -11,7 +11,11 @@ import ollama
 
 prompt = ''' Question: 
 
-How many maximum runs a batting team and a batter individually - can score in an over with 2 no balls. Think step by step, consider max runs per legal and no ball as 6 and consider 1 extra run per no ball. Consider extras runs only for teams total, those shall not be added in batters score.
+How many maximum runs a batting team and a batter individually - 
+can score in an over with 2 no balls. 
+Think step by step, consider max runs per legal and no ball as 6 and consider 1 extra run per no ball. 
+Consider extras runs only for teams total, 
+those shall not be added in batters score.
 '''
 
 response = ollama.chat(
