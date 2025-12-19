dc1d2975239d3054e0142fda8e1ffc67e9e22afa
ShivakrishnaD <shivakrishnadss1999@gmail.com>
2025-12-19 07:01:40 +0530

    Zero shot example with comments

--- zero_shot.py
+++ zero_shot.py
@@ -1,4 +1,10 @@
 import ollama
+# To run these on premise models, you need to have ollama server running locally.
+# Commands to run ollama server:
+# ollama pull llama3 ---> Once you pull the model, it will be available locally.
+# ollama run llama3 ---> To run the server with llama3 model.
+# ollama run gpt-oss ---> To run the server with gpt-oss model. Which is taking 13GB.
+
 
 response = ollama.chat(
     model="llama3", #LLM model 
